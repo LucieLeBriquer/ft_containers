8c6a5affd96cba507467f0b509b5221c496f4285
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-05-11 12:13:17 +0200

    pair

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 17:00:13 by lle-briq          #+#    #+#             */
-/*   Updated: 2022/05/11 11:44:52 by lle-briq         ###   ########.fr       */
+/*   Updated: 2022/05/11 12:11:01 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,10 @@
 #define END "\x1B[0m"
 #ifdef USESTL
 # define NSP std
+# define ISFT 0
 #else
 # define NSP ft
+# define ISFT 1
 #endif
 #define SIZE 60
 
@@ -40,33 +42,11 @@
 
 int	main(void)
 {
-	std::vector<int>::iterator	it;
-	std::vector<int>::iterator	it2;
-	std::string	str1 = "coucou";
-	std::string	str2 = "cougou";
-	ft::is_integral<int> t;
-	ft::pair<int, float>	p(4,2.5);
-
-
-	std::vector<int>	v(10);
-	std::vector<int>	v2(11);
-
-	std::cout << std::equal(v.begin(), v.end(), v2.begin()) << std::endl;
-	std::cout << ft::equal(v.begin(), v.end(), v2.begin()) << std::endl;
-	std::cout << ft::equal(str1.c_str(), str1.c_str() + 6, str2.c_str()) << std::endl;
-	ft::pair<int, float> p2(2, 3.45);
-	std::cout << p2.first << " " << p2.second << std::endl;
-	std::cout << (p2 == p) << std::endl;
-	p2 = p;
-	std::cout << p2.first << " " << p2.second << std::endl;
-	std::cout << (p2 == p) << std::endl;
-
-	it = v.begin();
-	while (it != v.end())
-	{
-		std::cout << *it << " ";
-		it++;
-	}
-	std::cout << std::endl;
+	if (ISFT)
+		std::cout << "=== USING FT ===" << std::endl << std::endl;
+	else
+		std::cout << "=== USING STD ===" << std::endl << std::endl;
+	NSP::pair<int, float> p = NSP::make_pair(3, 'a');
+	std::cout << p.first << " " << p.second << std::endl;
 	return (0);
 }
