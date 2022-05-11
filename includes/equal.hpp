de8ae8426d31cb88bc3de06d295d07d095677cf9
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-05-11 12:20:39 +0200

    equal

--- includes/equal.hpp
+++ includes/equal.hpp
@@ -6,7 +6,7 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 18:35:09 by lle-briq          #+#    #+#             */
-/*   Updated: 2022/05/11 11:40:48 by lle-briq         ###   ########.fr       */
+/*   Updated: 2022/05/11 12:17:32 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,37 @@ namespace ft
 					&& are_same<ValueType1, ValueType2>::value);
 
 		return (equal_aux<simple>::equal(first1, last2, first2));
+	}
+
+	// lexicographical compare
+	template <class It1, class It2>
+  	bool lexicographical_compare(It1 first1, It1 last1, It2 first2, It2 last2)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *first2 < *first1)
+				return false;
+			else if (*first1 < *first2)
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}
+
+	template <class It1, class It2, class Comp>
+  	bool lexicographical_compare(It1 first1, It1 last1, It2 first2, It2 last2, Comp comp)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || comp(*first2, *first1))
+				return false;
+			else if (comp(*first1, *first2))
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
 	}
 }
 
