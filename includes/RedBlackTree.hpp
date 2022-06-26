70bceca97bd9324a3e8c7973bf7e30b8e9e2b2c1
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-06-26 19:30:40 +0200

    starting RBT

--- includes/RedBlackTree.hpp
+++ includes/RedBlackTree.hpp
@@ -6,7 +6,7 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 17:12:47 by lle-briq          #+#    #+#             */
-/*   Updated: 2022/06/26 19:03:36 by lle-briq         ###   ########.fr       */
+/*   Updated: 2022/06/26 19:30:03 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,9 @@ namespace ft
 	{
 		private:
 			typedef T value_type;
-			typedef	Node *NodeP;
+			typedef	Node<T> *NodeP;
 			NodeP	_root;
+			NodeP	_leaf;
 		
 			void	_newNullNode(NodeP node, NodeP parent)
 			{
@@ -54,26 +55,78 @@ namespace ft
 				node->color = BLACK;
 			}
 
-			bool	_isLeaf(NodeP node)
-			{
-				return (node->left == NULL && node->right == NULL && node->color == BLACK);
-			}
-
 			NodeP	_searchNode(NodeP node, value_type value)
 			{
-				if (_isLeaf(node) || value == node->value)
+				if (node == _leaf || value == node->value)
 					return (node);
 				if (value < node->value)
 					return (_searchNode(node->left, value));
 				return (_searchNode(node->right, value));
 			}
 
+			void	_insertionUpdate(NodeP node)
+			{
+				NodeP	cur;
+
+				while (node->parent->color == RED)
+				{
+					if (node->parent == node->parent->parent->right)
+					{
+						cur = node->parent->parent->left;
+						if (cur->color == RED)
+						{
+							cur->color = BLACK;
+							node->parent->color = BLACK;
+							node->parent->parent->color = RED;
+							node = node->parent->parent;
+						}
+						else
+						{
+							if (node == node->parent->left)
+							{
+								node = node->parent;
+								rotateRight(node);
+							}
+							node->parent->color = BLACK;
+							node->parent->parent->color = RED;
+							rotateLeft(node->parent->parent);
+						}
+					}
+					else
+					{
+						cur = node->parent->parent->right;
+						if (cur->color == RED)
+						{
+							cur->color = BLACK;
+							node->parent->color = BLACK;
+							node->parent->parent->color = RED;
+							node = node->parent->parent;
+						}
+						else
+						{
+							if (node == node->parent->right)
+							{
+								node = node->parent;
+								rotateLeft(node);
+							}
+							node->parent->color = BLACK;
+							node->parent->parent->color = RED;
+							rotateRight(node->parent->parent);
+						}
+					}
+					if (node == _root)
+						break;
+				}
+				_root->color = BLACK;
+			}
+
 		public:
 			// constructors
 			RedBlackTree()
 			{
-				_root = new Node;
-				_newLeaf(_root);
+				_leaf = new Node<T>;
+				_newLeaf(_leaf);
+				_root = _leaf;
 			}
 
 			~RedBlackTree()
@@ -88,14 +141,14 @@ namespace ft
 			
 			NodeP	minimum(NodeP node)
 			{
-				while (!_isLeaf(node->left))
+				while (node->left != _leaf)
 					node = node->left;
 				return (node);
 			}
 
 			NodeP	maximum(NodeP node)
 			{
-				while (!_isLeaf(node->right))
+				while (node->right != _leaf)
 					node = node->right;
 				return (node);
 			}
@@ -104,10 +157,10 @@ namespace ft
 			{
 				NodeP	cur;
 
-				if (!_isLeaf(node->right))
+				if (node->right != _leaf)
 					return (minimum(node->right));
 				cur = node->parent;
-				while (!_isLeaf(cur) && node == cur->right)
+				while (cur != _leaf && node == cur->right)
 				{
 					node = cur;
 					cur = cur->parent;
@@ -119,15 +172,95 @@ namespace ft
 			{
 				NodeP	cur;
 
-				if (!_isLeaf(node->left))
+				if (node->left != _leaf)
 					return (maximum(node->left));
 				cur = node->parent;
-				while (!_isLeaf(cur) && node == cur->left)
+				while (cur != _leaf && node == cur->left)
 				{
 					node = cur;
 					cur = cur->parent;
 				}
 				return (cur);
+			}
+
+			void	rotateLeft(NodeP node)
+			{
+				NodeP	save;
+
+				save = node->right;
+				node->right = save->left;
+				if (save->left != _leaf)
+					save->left->parent = node;
+				save->parent = node->parent;
+				if (node->parent == NULL)
+					_root = save;
+				else if (node == node->parent->left)
+					node->parent->left = save;
+				else
+					node->parent->right = save;
+				save->left = node;
+				node->parent = save;
+			}
+
+			void	rotateRight(NodeP node)
+			{
+				NodeP	save;
+
+				save = node->left;
+				node->left = save->right;
+				if (save->right != _leaf)
+					save->right->parent = node;
+				save->parent = node->parent;
+				if (node->parent == NULL)
+					_root = save;
+				else if (node == node->parent->right)
+					node->parent->right = save;
+				else
+					node->parent->left = save;
+				save->right = node;
+				node->parent = save;
+			}
+
+			void	insert(value_type value)
+			{
+				NodeP	node = new Node<T>;
+				NodeP	cur;
+				NodeP	root;
+
+				node->parent = NULL;
+				node->value = value;
+				node->left = _leaf;
+				node->right = _leaf;
+				node->color = RED;
+
+				cur = NULL;
+				root = _root;
+				while (cur != _leaf)
+				{
+					root = cur;
+					if (node->value < cur->value)
+						cur = cur->left;
+					else
+						cur = cur->right;
+				}
+
+				node->parent = root;
+				if (root == NULL)
+					root = node;
+				else if (node->value < root->value)
+					root->left = node;
+				else
+					root->right = node;
+				
+				if (node->parent == NULL)
+				{
+					node->color = BLACK;
+					return ;
+				}
+				if (node->parent->parent == NULL)
+					return ;
+				
+				_insertionUpdate(node);
 			}
 	};
 }
--- sources/main.cpp
+++ sources/main.cpp
@@ -6,17 +6,17 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 17:00:13 by lle-briq          #+#    #+#             */
-/*   Updated: 2022/05/31 20:55:56 by lle-briq         ###   ########.fr       */
+/*   Updated: 2022/06/26 19:27:30 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.hpp"
-#include "map.hpp"
+//#include "map.hpp"
+#include "RedBlackTree.hpp"
 #include <iostream>
 #include <iomanip>
 #include <vector>
 #include <map>
-#define RED "\x1B[31m"
 #define GREEN "\x1B[32m"
 #define YELLOW "\x1B[33m"
 #define ORANGE "\x1B[34m"
@@ -33,40 +33,13 @@
 #endif
 #define SIZE 60
 
-namespace ft
-{
-	template<typename K, typename T>
-	struct test
-	{
-		bool operator()(const ft::pair<K, T> & a, const ft::pair<K, T> & b) const
-		{
-			return (a.first < b.first);
-		}
-	};
-}
 
 int		main(void)
 {
-	ft::binarySearchTree< ft::pair<int, std::string>, ft::test<int, std::string> > bst;
+	ft::RedBlackTree<int>	tree;
 
-	bst.insert(ft::make_pair<int, std::string>(2, "deux"));
-	bst.insert(ft::make_pair<int, std::string>(1, "un"));
-	bst.insert(ft::make_pair<int, std::string>(3, "trois"));
-	bst.insert(ft::make_pair<int, std::string>(4, "deuttx"));
-	bst.insert(ft::make_pair<int, std::string>(5, "utrn"));
-	bst.insert(ft::make_pair<int, std::string>(6, "trotrbis"));
-	bst.insert(ft::make_pair<int, std::string>(8, "derbtux"));
-	bst.insert(ft::make_pair<int, std::string>(7, "unbrtrbt"));
-	bst.insert(ft::make_pair<int, std::string>(9, "troisze"));
-	bst.insert(ft::make_pair<int, std::string>(10, "blegh"));
-	std::cout << bst.research(ft::make_pair<int, std::string>(1, ""))->value.second << std::endl;
-	std::cout << bst.research(ft::make_pair<int, std::string>(2, ""))->value.second << std::endl;
-	std::cout << bst.research(ft::make_pair<int, std::string>(3, ""))->value.second << std::endl;
+	tree.insert(1);
+	tree.insert(3);
+	tree.insert(2);
 
-	bst.remove(ft::make_pair<int, std::string>(5, ""));
-	std::cout << bst.research(ft::make_pair<int, std::string>(4, "")) << std::endl;
-	
-	ft::map<char, int>	mymap;
-
-	mymap.insert(ft::pair<char,int>('a',100));
 }
